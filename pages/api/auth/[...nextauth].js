7bac227d82109371e6d825a314560639e79b41d3
EmmanuelMadanha <raymadanha@gmail.com>
2023-06-09 10:03:18 +0200

    Initial commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['dawid.paszko@gmail.com'];
+const adminEmails = ['aithinktank22@gmail.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 import mime from 'mime-types';
 import {mongooseConnect} from "@/lib/mongoose";
 import {isAdminRequest} from "@/pages/api/auth/[...nextauth]";
-const bucketName = 'dawid-next-ecommerce';
+const bucketName = 'travelerharare';
 
 export default async function handle(req,res) {
   await mongooseConnect();
@@ -19,7 +19,7 @@ export default async function handle(req,res) {
   });
   console.log('length:', files.file.length);
   const client = new S3Client({
-    region: 'us-east-1',
+    region: 'eu-north-1',
     credentials: {
       accessKeyId: process.env.S3_ACCESS_KEY,
       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
